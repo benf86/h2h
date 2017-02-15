2f2ca0c3facfe20bf3c4e0ef894e0d534ba43be5
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-02-15 17:30:28 +0100

    fix user creation and getting

--- repositories/base.js
+++ repositories/base.js
@@ -1,5 +1,6 @@
 module.exports = globals => modelName => ({
-  get: meta => data => globals.db(`${modelName}s`).where(meta || true),
+  get: meta => data => globals.db(`${modelName}s`).where(meta || true).orWhere({ slack_name: meta.id })
+    .orWhere({ silly_name: meta.id }),
   create: meta => data => globals.db(`${modelName}s`).insert(data),
   update: meta => data => globals.db(`${modelName}s`).where(meta).update(data),
   delete: meta => data => globals.db(`${modelName}s`).where(meta).delete(),
--- services/user.js
+++ services/user.js
@@ -4,10 +4,11 @@ const { User } = require('../models/user');
 
 module.exports = globals => _.merge({}, baseServices(globals)('user'), {
   create: params => query => body =>
-    globals.repositories.user.get({ slack_name: body.slack_name })()
+    globals.repositories.user.get({ id: body.slack_name })()
       .then(existingUser => (existingUser[0]
-          ? existingUser[0]
+          ? existingUser
           : globals.repositories.user
-            .create()(new User(_.merge({}, body))).then(newUser => globals.repositories.user.get({ id: newUser[0] })())
+            .create()(new User(_.merge({}, body)))
+            .then(newUser => globals.repositories.user.get({ id: newUser[0] })())
       )),
 });
